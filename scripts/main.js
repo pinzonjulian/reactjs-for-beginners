31f9aa13874fd355b5ddf03fd63155aab6e1dda5
pinzonjulian <julianpinzoneslava@gmail.com>
2015-11-07 16:56:36 -0500

    16 of 20. Persisting state with local storage

--- scripts/main.js
+++ scripts/main.js
@@ -43,6 +43,16 @@ var App = React.createClass ({
       context: this,
       state: 'fishes'
     });
+    var localStorageRef = localStorage.getItem('order-' +  this.props.params.storeId)
+
+    if(localStorageRef){
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      })
+    }
+  },
+  componentWillUpdate : function(nextProps, nextState){
+    localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
   },
   addToOrder : function (key){
     this.state.order[key] = this.state.order[key] + 1 || 1;
@@ -191,7 +201,7 @@ var Order = React.createClass({
     }
 
     return(
-      <li>
+      <li key={key}>
         <span>{count}</span>lbs
         {fish.name}
         <span className="price">{h.formatPrice(count * fish.price)}</span>
